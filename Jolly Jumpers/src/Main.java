a04b83e96552995386d370ade2c4f87e98565068
Brogan <brogancle@gmail.com>
2017-09-08 12:50:43 -0500

    Start Jolly Jumpers

--- Jolly Jumpers/src/Main.java
+++ Jolly Jumpers/src/Main.java
@@ -1,6 +1,26 @@
+import java.io.*;
+
 public class Main {
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args){
+    static String[] lineIn;
+    static int[] currentInts;
 
+    static boolean[] jollys;
+    public static void main(String[] args) throws IOException{
+        String in;
+        while((in = input.readLine()) != null && !in.equals("")){
+            lineIn = in.split(" ");
+            currentInts = new int[lineIn.length];
+            for (int i = 0; i < currentInts.length; i++){
+                currentInts[i] = Integer.valueOf(lineIn[i]);
+            }
+            jollys = new boolean[currentInts.length-1];
+            boolean isJolly = false;
+            for(int i = 0; i < jollys.length; i++){
+                int difference = Math.abs(currentInts[i] - currentInts[i+1]);
+            }
+        }
     }
 }
