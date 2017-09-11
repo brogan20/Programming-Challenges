b1c0397a8be062a53ef754d31470abd42512d94f
Brogan <brogancle@gmail.com>
2017-09-11 12:44:05 -0500

    Jolly Jumpers Complete #19990615

--- Jolly Jumpers/src/Main.java
+++ Jolly Jumpers/src/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 
 public class Main {
-    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     static String[] lineIn;
@@ -12,14 +11,31 @@ public class Main {
         String in;
         while((in = input.readLine()) != null && !in.equals("")){
             lineIn = in.split(" ");
-            currentInts = new int[lineIn.length];
+            currentInts = new int[Integer.valueOf(lineIn[0])];
+
             for (int i = 0; i < currentInts.length; i++){
-                currentInts[i] = Integer.valueOf(lineIn[i]);
+                currentInts[i] = Integer.valueOf(lineIn[i+1]);
             }
-            jollys = new boolean[currentInts.length-1];
-            boolean isJolly = false;
+            jollys = new boolean[Integer.valueOf(lineIn[0])-1];
+            for(int i = -1; i < jollys.length-1; i++){
+                int difference = Math.abs(currentInts[i+1] - currentInts[i+2])-1;
+
+                if (difference >= 0 && difference < jollys.length){
+                    jollys[difference] = true;
+                }
+
+            }
+            boolean isJolly = true;
             for(int i = 0; i < jollys.length; i++){
-                int difference = Math.abs(currentInts[i] - currentInts[i+1]);
+                if (!jollys[i]){
+                    isJolly = false;
+                    break;
+                }
+            }
+            if(isJolly){
+                System.out.println("Jolly");
+            } else {
+                System.out.println("Not jolly");
             }
         }
     }
