5c0132c169562fe36832b979ef1cff9f51b9a67b
brogan20 <31775601+brogan20@users.noreply.github.com>
2017-09-15 12:25:18 -0500

    Hartals

--- Hartals/src/Main.java
+++ Hartals/src/Main.java
@@ -5,6 +5,10 @@ public class Main {
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args){
-
+        int T = Integer.parseInt(input.readLine().trim());
+        for(int i = 0; i < T; i++){
+            int days = Integer.parseInt(input.readLine().trim());
+            
+        }
     }
 }
