dd660f84b17a0c630a329761c5582d582d6cd137
Brogan <brogancle@gmail.com>
2017-09-20 12:50:45 -0500

    Poker Hands

---
+++ Poker Hands/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException{
+        String line;
+        while ((line = input.readLine()) != null){
+            String
+        }
+    }
+}
