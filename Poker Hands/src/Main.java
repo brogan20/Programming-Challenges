e293c596f378af6dbaee268bebc771339472ca6d
Brogan <brogancle@gmail.com>
2017-09-21 12:48:41 -0500

    Poker Hands

--- Poker Hands/src/Main.java
+++ Poker Hands/src/Main.java
@@ -5,7 +5,16 @@ public class Main {
     public static void main(String[] args) throws IOException{
         String line;
         while ((line = input.readLine()) != null){
-            String
+            String[] inLine = line.split(" ");
+            //Black
+            int black = 0;
+            for(int i = 0; i < 1; i++){
+                //1 Pair
+                for(int j = 0; j < 5; j++){
+
+                }
+
+            }
         }
     }
 }
