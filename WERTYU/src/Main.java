8da10e25a4d47595b414a3c769277f126cf42d8d
Brogan <brogancle@gmail.com>
2017-09-25 12:50:51 -0500

    WERTYU

---
+++ WERTYU/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    char[] qwerty = {'`','1','2','3','4','5','6','7','8','9','0','-',
+                     '=','Q','W','E','R','T','Y','U','I','O','P','[',
+                     ']','\\','A','S','D','F','G','H','J','K','L',';',
+                     '\'', 'Z','X','C','V','B','N','M',',','.','/'};
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+        while (input.hasNext()){
+            char next = input.next().charAt(0);
+            if (next == 'Q'){
+
+            }
+        }
+    }
+}
