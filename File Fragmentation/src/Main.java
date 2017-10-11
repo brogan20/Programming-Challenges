d899b57e329299dd2738f2253313ae5af245b043
Brogan <brogancle@gmail.com>
2017-10-10 23:06:13 -0500

    File Fragmentation

--- File Fragmentation/src/Main.java
+++ File Fragmentation/src/Main.java
@@ -15,29 +15,53 @@ public class Main {
             frags = new String[288];
             String in;
 
+            int fragsSize = 0;
             int low = Integer.MAX_VALUE;
             int high = 0;
 
+            //Get all the inputs
             while (!(in = input.readLine()).equals("")){
-                if(!in.equals("")){
-                    if(in.length() < low){
-                        low = in.length();
-                    }else if(in.length() > high){
-                        high = in.length();
+                if(in.length() < low){
+                    low = in.length();
+                }else if(in.length() > high){
+                    high = in.length();
+                }
+                frags[fragsSize] = in;
+                fragsSize++;
+            }
+
+            //Create a matrix of the fragments
+            int length = low + high;
+            char[][] charMap = new char[fragsSize*2][length];
+            int currRow = 0;
+            for (int i = 0; i < fragsSize; i++) {
+                for (int j = 0; j < frags[i].length(); j++){
+                    charMap[currRow][j] = frags[i].charAt(j);
+                    charMap[currRow+1][length-frags[i].length()+j] = frags[i].charAt(j);
+                }
+                currRow += 2;
+            }
+            //Sum up each column
+            int[] output = new int[length];
+            int ones = 0;
+            int zeros = 0;
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; j < charMap.length; j++) {
+                    if(charMap[j][i] == '0'){
+                        zeros++;
+                    } else {
+                        ones++;
                     }
-                    frags[caseNum] = in;
+                }
+                if (zeros >= ones){
+                    output[i] = 0;
                 } else {
-                    break;
+                    output[i] = 1;
                 }
             }
 
-            int length = low + high;
-            char[][] thing = new char[576][length];
-            int[] output = new int[length];
 
-
-
-            //Outputs
+            //Output the findings
             for (int i : output) {
                 System.out.print(i);
             }
