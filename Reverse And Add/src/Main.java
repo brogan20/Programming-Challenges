cf30ed6bb2fda1a9158affa86eff3687357122e8
Brogan <brogancle@gmail.com>
2017-11-03 12:13:47 -0500

    Reverse and Add complete 20293747 - 0.120s

--- Reverse And Add/src/Main.java
+++ Reverse And Add/src/Main.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.StringBuilder;
 
 public class Main {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException{
-        byte cases = Byte.parseByte(input.readLine());
+        short cases = Short.parseShort(input.readLine());
 
-        for (byte case_ = 0; case_ < cases; case_++) {
+        for (short case_ = 0; case_ < cases; case_++) {
             long iterations = 0;
-            String current = input.readLine().trim();
+            String current = input.readLine();
             String reverse = new StringBuilder(current).reverse().toString();
             do {
                 iterations++;
-                long currentInt = Long.parseLong(current);
-                long reverseInt = Long.parseLong(reverse);
-                current = Long.toString(currentInt + reverseInt);
-            } while ((!current.equals(reverse = new StringBuilder(current).reverse().toString())));
+                current = Long.toString(Long.parseLong(current) + Long.parseLong(reverse));
+            } while (!current.equals(reverse = new StringBuilder(current).reverse().toString()));
             System.out.println(iterations + " " + current);
         }
     }
