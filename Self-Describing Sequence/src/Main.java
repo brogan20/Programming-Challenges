0fd084295d9d82bc319e20da1cefe905e85677f1
Brogan <brogancle@gmail.com>
2017-11-08 12:48:51 -0600

    Self Describing Sequence

---
+++ Self-Describing Sequence/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main (String[] args) throws IOException{
+        String in;
+        while (!(in = input.readLine()).equals("0")){
+            int current = Integer.parseInt(in);
+
+        }
+    }
+}
