bfdbee553cc0c378b3895ef119dfb3265e3ec26f
Brogan <brogancle@gmail.com>
2017-11-11 19:55:43 -0600

    File Fragmentation 20333730 - 0.040s

--- File Fragmentation/src/Main.java
+++ File Fragmentation/src/Main.java
@@ -1,51 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static Scanner input = new Scanner(System.in);
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args){
-        int cases = Integer.parseInt(input.nextLine());
-
+    public static void main(String[] args) throws IOException {
+        //1st line is the case number
+        int cases = Integer.parseInt(input.readLine());
+        input.readLine();
         for (int caseNum = 0; caseNum < cases; caseNum++) {
-            ArrayList<String> frags = new ArrayList<>();
+            //Get all fragments and find the longest and shortest lengths
+            //The sum of those is the size the output has to be
             String in;
-            int high = -1;
-            int low = Integer.MAX_VALUE;
-            while (!(in = input.nextLine()).equals("")){
-                if(in.length() > high) high = in.length();
-                if(in.length() < low) low = in.length();
+            ArrayList<String> frags = new ArrayList<>();
+            int longest = -1;
+            int shortest = 300;
+            while ((in = input.readLine()) != null && !in.equals("")) {
                 frags.add(in);
+                if (in.length() > longest) longest = in.length();
+                if (in.length() < shortest) shortest = in.length();
             }
+            int size = longest + shortest;
 
-            int maxLength = high + low;
-            Map<String, Integer> combos = new HashMap<>();
-            for (String fragOne : frags) {
-                for (String fragTwo : frags) {
-                    int length = fragOne.length() + fragTwo.length();
-                    if (fragOne != fragTwo && length == maxLength) {
-                        String concat = fragOne + fragTwo;
-                        if (combos.containsKey(concat)) {
-                            combos.replace(concat, combos.get(concat) + 1);
+            //Find all possible combinations of valid length
+            Map<String, Integer> combinations = new HashMap<>();
+            for (int i = 0; i < frags.size(); i++) {
+                for (int j = i+1; j < frags.size(); j++) {
+                    if(frags.get(i).length() + frags.get(j).length() == size){
+                        String comboOne = frags.get(i) + frags.get(j);
+                        String comboTwo = frags.get(j) + frags.get(i);
+                        if (combinations.containsKey(comboOne)){
+                            combinations.replace(comboOne, combinations.get(comboOne) + 1);
                         } else {
-                            combos.put(concat, 1);
+                            combinations.put(comboOne, 1);
+                        }
+                        if (combinations.containsKey(comboTwo)){
+                            combinations.replace(comboTwo, combinations.get(comboTwo) + 1);
+                        } else {
+                            combinations.put(comboTwo, 1);
                         }
                     }
                 }
             }
 
-            int highNum = -1;
-            String value = "";
-            for (Map.Entry<String, Integer> combo : combos.entrySet()) {
-                if (combo.getValue() > highNum){
-                    highNum = combo.getValue();
-                    value = combo.getKey();
+            //The combination that occurred most frequently is the solution
+            String commonCombo = "";
+            int frequency = 0;
+            for (String key : combinations.keySet()){
+                if (!key.equals(commonCombo) && combinations.get(key) > frequency){
+                    frequency = combinations.get(key);
+                    commonCombo = key;
                 }
             }
-            if (caseNum > 0) {
-                System.out.println();
-            }
-            System.out.println(value.trim());
 
+            if (caseNum > 0) System.out.println("\n"+commonCombo.trim());
+            else System.out.println(commonCombo.trim());
         }
     }
 }
