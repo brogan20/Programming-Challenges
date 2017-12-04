c1c0e49d12ff1aed2156fc240e88379c543309a2
Brogan <brogancle@gmail.com>
2017-12-04 12:22:14 -0600

    Unsigned integers are superior in speed and thus have been implemented for Carmichael Numbers. Also garden of eden was started

--- Carmichael Numbers/src/Main.java
+++ Carmichael Numbers/src/Main.java
@@ -3,20 +3,24 @@ import java.io.*;
 public class Main {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
     static int[] nums = {561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341, 41041, 46657, 52633, 62745, 63973};
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         String in;
-        while (!(in = input.readLine()).equals("0")){
-            int n = Integer.parseInt(in);
+        while (!(in = input.readLine()).equals("0")) {
+            int n = Integer.parseUnsignedInt(in);
             boolean found = false;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == n){
+            for (int num : nums) {
+                if (num == n) {
                     found = true;
                     output.write("The number " + n + " is a Carmichael number.\n");
                     break;
                 }
             }
-            if (!found) output.write(n + " is normal.\n");
+            if (!found) {
+                output.write(n + " is normal.\n");
+            }
         }
         output.close();
     }
---
+++ Garden of Eden/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException{
+        String in;
+        while ((in = input.readLine()) != null && !in.equals("")){
+            
+        }
+    }
+}
