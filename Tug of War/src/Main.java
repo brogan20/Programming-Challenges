714450c064fd4e4317038a230098878e4cb6daf5
Brogan <brogancle@gmail.com>
2017-12-21 12:50:01 -0600

    Tug of War

--- Tug of War/src/Main.java
+++ Tug of War/src/Main.java
@@ -1,15 +1,31 @@
 import java.io.*;
 
+
 public class Main {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException{
         int cases = Integer.parseUnsignedInt(input.readLine());
-        //skip blank line
+        //Pass blank line
         input.readLine();
-        for (int case_ = 0; case_ < cases; case_++) {
+        while (cases-- > 0){
+            int numPeeps = Integer.parseUnsignedInt(input.readLine());
+            int[] weights = new int[numPeeps];
+            int maxWeight = 0;
+            for (int i = 0; i < weights.length; i++) {
+                int currWeight = Integer.parseUnsignedInt(input.readLine());
+                weights[i] = currWeight;
+                maxWeight += currWeight;
+            }
 
+            long[] solutions = new long[maxWeight +1];
+            solutions[0] = 1;
+            for (int i = 0; i < numPeeps; i++) {
+                for (int j = 0; j < maxWeight; j++) {
+                    solutions[j] |= (solutions[j - weights[i]]) << 1;
+                }
+            }
         }
     }
 }
